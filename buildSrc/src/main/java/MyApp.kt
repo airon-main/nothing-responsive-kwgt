9ff8b6798683ed495e7c55c89cd3cbbc7116a938
airon-main <airon.pribadi@gmail.com>
2025-02-07 19:07:21 +0700

    initial commit

---
+++ buildSrc/src/main/java/MyApp.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object MyApp {
+    const val appId = "dev.jahir.kuper.app"
+    const val version = 253
+    const val versionName = "2.5.3"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.6.1"
+    const val kotlin = "2.1.0"
+    const val ksp = "$kotlin-1.0.29"
+
+    // OneSignal
+    const val oneSignal = "4.8.10"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 35
+    const val buildTools = "35.0.0"
+
+    // Kuper
+    const val kuper = "2.5.3"
+}
